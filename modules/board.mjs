78048ca37d2edc1c11e566a57155adf0b2adb123
Victor <victorshiira4869@gmail.com>
2023-03-28 14:04:45 -0300

    Create coordinateToVertex method on Board class

--- modules/board.mjs
+++ modules/board.mjs
@@ -3,7 +3,10 @@ import Graph from "./graph.mjs";
 const Board = () => {
   const boardGraph = Graph();
 
+  let currentBoardSize = 0;
+
   const generateBoard = (boardSize) => {
+    currentBoardSize = boardSize;
     boardGraph.adjacencyList.clear();
     for (let i = 0; i < boardSize ** 2; i++) {
       boardGraph.addVertex(i);
@@ -24,25 +27,26 @@ const Board = () => {
     [2, 1],
   ];
 
-  const setVertexEdges = (x, boardSize) => {
-    const row = Math.floor(x / boardSize);
-    const column = x % boardSize;
+  const setVertexEdges = (x) => {
+    const row = Math.floor(x / currentBoardSize);
+    const column = x % currentBoardSize;
 
     for (const move of validMoves) {
       const targetRow = row + move[0];
       const targetColumn = column + move[1];
-      if (
-        isAValidSquare(targetRow, boardSize) &&
-        isAValidSquare(targetColumn, boardSize)
-      ) {
-        const targetVertex = targetRow * boardSize + targetColumn;
+      if (isAValidSquare(targetRow) && isAValidSquare(targetColumn)) {
+        const targetVertex = coordinateToVertex(targetRow, targetColumn);
         boardGraph.addEdge(x, targetVertex);
       }
     }
   };
 
-  const isAValidSquare = (x, boardSize) => {
-    return x >= 0 && x < boardSize;
+  const isAValidSquare = (x) => {
+    return x >= 0 && x < currentBoardSize;
+  };
+
+  const coordinateToVertex = (row, column) => {
+    return row * currentBoardSize + column;
   };
 
   return { generateBoard };
